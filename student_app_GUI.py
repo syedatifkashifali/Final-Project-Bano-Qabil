81dd87a5c24074fd50977687ad2497557cf622cf
Syed Atif Ali <corruptedgamerz977@gmail.com>
2024-03-02 06:35:36 -0800

    Update student_app_GUI.py

--- student_app_GUI.py
+++ student_app_GUI.py
@@ -397,7 +397,7 @@ def contact():
 def main():
     
     # Company logo
-    st.sidebar.image("download.png", use_column_width=True)
+    st.sidebar.image("download.png", use_column_width=True, height = 200px)
     
     # Create a navigation bar
     nav = st.sidebar.radio("Navigation", ["Home", "About", "Contact"])
